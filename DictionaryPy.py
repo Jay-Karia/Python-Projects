e754d1cc33d0364b19f3efa7c9cb340a384d81b9
Jay <86977383+Jay-Karia@users.noreply.github.com>
2021-07-06 21:51:33 +0530

    Add files via upload

---
+++ DictionaryPy.py
@@ -0,0 +1,14 @@
+d1 = {
+    "Jay": "Pizza",
+    "Rohan": "Sweets",
+    "Mahu": "Roti",
+    "Rudra": {
+        "Morning": "Maggi",
+        "Afternoon": "Chicken",
+        "Night": "Rice"
+    }
+}
+d1["Ankit"] = "Burgers"
+d1["120"] = "Rasgulla"
+del d1["120"]
+print(d1)
