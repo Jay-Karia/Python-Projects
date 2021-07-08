8aebc43ab91cd225319359e929e884fc791084f4
Jay <jay.sanjay.karia@gmail.com>
2021-07-08 17:25:53 +0530

    Add files via upload

---
+++ Average Generator/Version 1.0/average.py
@@ -0,0 +1,92 @@
+print("Welcome to Python Average Generator v-2.0 -- By Jay")
+print("\nBeware Exceptions are not handled, it would be in v-2.1")
+print("Enter how many numbers do you want to write")
+numbers = int(input())
+
+if numbers == 1:
+    n1 = int(input("\nEnter number 1: "))
+    avg = n1 / 1
+    print("The Average is: ", avg)
+elif numbers == 2:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    avg = (n1+n2)/2
+    print("The Average is: ", avg)
+elif numbers == 3:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    avg = (n1+n2+n3)/3
+    print("The Average is: ", avg)
+elif numbers == 4:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    avg = (n1+n2+n3+n4)/4
+    print("The Average is: ", avg)
+elif numbers == 5:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    n5 = int(input("Enter number 5: "))
+    avg = (n1+n2+n3+n4+n5)/5
+    print("The Average is: ", avg)
+elif numbers == 6:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    n5 = int(input("Enter number 5: "))
+    n6 = int(input("Enter number 6: "))
+    avg = (n1+n2+n3+n4+n5+n6)/6
+    print("The Average is: ", avg)
+elif numbers == 7:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    n5 = int(input("Enter number 5: "))
+    n6 = int(input("Enter number 6: "))
+    n7 = int(input("Enter number 7: "))
+    avg = (n1+n2+n3+n4+n5+n6+n7)/7
+    print("The Average is: ", avg)
+elif numbers == 8:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    n5 = int(input("Enter number 5: "))
+    n6 = int(input("Enter number 6: "))
+    n7 = int(input("Enter number 7: "))
+    n8 = int(input("Enter number 8: "))
+    avg = (n1+n2+n3+n4+n5+n6+n7+n8)/8
+    print("The Average is: ", avg)
+elif numbers == 9:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    n5 = int(input("Enter number 5: "))
+    n6 = int(input("Enter number 6: "))
+    n7 = int(input("Enter number 7: "))
+    n8 = int(input("Enter number 8: "))
+    n9 = int(input("Enter number 9: "))
+    avg = (n1+n2+n3+n4+n5+n6+n7+n8+n9)/9
+    print("The Average is: ", avg)
+elif numbers == 10:
+    n1 = int(input("\nEnter number 1: "))
+    n2 = int(input("Enter number 2: "))
+    n3 = int(input("Enter number 3: "))
+    n4 = int(input("Enter number 4: "))
+    n5 = int(input("Enter number 5: "))
+    n6 = int(input("Enter number 6: "))
+    n7 = int(input("Enter number 7: "))
+    n8 = int(input("Enter number 8: "))
+    n9 = int(input("Enter number 9: "))
+    n10 = int(input("Enter number 10: "))
+    avg = (n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)/10
+    print("The Average is: ", avg)
+else:
+    print("Only the average of 10 numbers are available")
