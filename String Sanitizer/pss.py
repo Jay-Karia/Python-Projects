906ec62710f9de7c339adf88faec37259d5e2db4
Jay <jay.sanjay.karia@gmail.com>
2021-07-09 09:38:53 +0530

    Update pss.py

--- String Sanitizer/pss.py
+++ String Sanitizer/pss.py
@@ -1,35 +1,39 @@
-# Python String Sanitizer
-def sanitize(time_string):
-    if '-' in time_string:
-        splitter = '-'
-    elif ':' in time_string:
-        splitter = ':'
-    elif ',' in time_string:
-        splitter = ','
+print("Welcome to Python String Sanitizer -- By Jay")
+user_string = input("\nEnter your String: ")
+print("\n1) Delete Commas(,)\n2) Delete Colons(:)\n3) Delete Whitespace( )")
+select = input()
+
+
+def delete_commas(string):
+    splitter = ','
+    final_string = string.replace(splitter, "")
+    print("\nFinal String: ", final_string)
+
+
+def delete_colons(string):
+    splitter = ':'
+    final_string = string.replace(splitter, "")
+    print("\nFinal String: ", final_string)
+
+
+def delete_whitespace(string):
+    splitter = ' '
+    final_string = string.replace(splitter, "")
+    print("\nFinal String: ", final_string)
+
+
+valid = True
+
+while valid:
+    if select == "1":
+        delete_commas(user_string)
+        break
+    elif select == "2":
+        delete_colons(user_string)
+        break
+    elif select == "3":
+        delete_whitespace(user_string)
+        break
     else:
-        return time_string
-    (mins, secs) = time_string.split(splitter)
-    return mins + " " + secs
-
-
-print("Enter your name")
-name = input()
-print("\nWelcome " + name + " to Python String Sanitizer -- By Jay\n")
-print("Enter a String")
-user_string = input()
-print("String: " + user_string)
-print("\nSanitizing...")
-print("\nSanitized!!!")
-print(sanitize(user_string))
-# print("\nEnter a Operation on your String")
-# print("1) Delete Commas\n2) Delete Full Stops\n3) Trim half of the String\n")
-# op = input()
-# for operation in op:
-#     if '1' in operation:
-#          print("\nDelete Commas: ")
-#         # (one, two) = user_string.split(',')
-#         # print(one + " " + two)
-#     elif '2' in operation:
-#         print("\nDelete Full Stops")
-#     elif '3' in operation:
-#         print("\nTrim Half of the String")
+        print("It should be a valid option!")
+        valid = False
