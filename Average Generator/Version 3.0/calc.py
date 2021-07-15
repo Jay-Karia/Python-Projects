3ebbab935535f538129ac9e525a920da1c56acd2
Jay <86977383+Jay-Karia@users.noreply.github.com>
2021-07-15 11:30:25 +0530

    100 % Done!!

--- Average Generator/Version 3.0/calc.py
+++ Average Generator/Version 3.0/calc.py
@@ -4,6 +4,8 @@ def avg(num_n):
     """
     This is a Function to Calculate the Average of Unlimited Numbers
     """
+    global Sum
+    Sum = 0
     nums = 0
     n = None
     for i in range(num_n):
@@ -12,14 +14,13 @@ def avg(num_n):
             n = (input(f"\nEnter number {i}: "))
             nums = int(n)
             print(f"{nums}")
-        except ValueError as ve:
+        except ValueError:
             nums = len(n)
             print(f"{nums}")
-        # nums/len(nums)
+        Sum = Sum + i
         pass
-    # average = 0.0
-    # print(f"\nAverage == {average}")
-    # print(f"N == {len(n)} nums == {nums}")
+    average = Sum / nums
+    print(f"\nAverage == {average}")
 num_nu = 0
 num = None
 try:
