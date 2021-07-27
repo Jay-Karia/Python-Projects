dda2e99ef85ed45be5737aba9a0cf87ca074f0b5
Jay <86977383+Jay-Karia@users.noreply.github.com>
2021-07-27 16:28:15 +0530

    new function

--- String Sanitizer/Version 2/santizer.py
+++ String Sanitizer/Version 2/santizer.py
@@ -1,7 +1,6 @@
 print("Welcome to Python String Sanitizer -- By Jay")
 user_string = input("\nEnter your String: ")
-print("\n1) Delete Commas(,)\n2) Delete Colons(:)\n3) Delete Whitespace( )")
-select = input()
+print("\n1) Delete Commas(,)\n2) Delete Colons(:)\n3) Delete Whitespace( )\n4) Custom Delete")
 
 
 def delete_commas(string):
@@ -21,10 +20,19 @@ def delete_whitespace(string):
     final_string = string.replace(splitter, "")
     print("\nFinal String: ", final_string)
 
+def custom_del(string):
+    ch = input("\nEnter character to be deleted: ")
+    splitter = ch
+    final_string = string.replace(splitter, "")
+    print("\nFinal String: ", final_string)
+    pass
+
 
 valid = True
 
-while valid:
+while valid == True:
+    select = input()
+    
     if select == "1":
         delete_commas(user_string)
         break
@@ -33,6 +41,9 @@ while valid:
         break
     elif select == "3":
         delete_whitespace(user_string)
+        break
+    elif select == "4":
+        custom_del(user_string)
         break
     else:
         print("It should be a valid option!")
